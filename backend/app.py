9a98ab96018130d5fa50180d2783a621415253af
Seth Emery <37260478+collaboratorsje@users.noreply.github.com>
2024-09-12 14:06:47 -0500

    Created react and flask CORS compatibility, need to change README now

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
@@ -8,3 +10,4 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
